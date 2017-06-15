91e4cc24b14753a129f493b05d2a7f560e2f15a4
kistorm <chaoqi_qc@sina.com>
2017-06-15 17:12:52 +0800

    merge
    merge dev to master

--- common/common.logger.js
+++ common/common.logger.js
@@ -10,8 +10,7 @@ var dateFormat = function () {
 }
 var ignoreEpipe = function (err) {
     return err.code !== 'EPIPE';
-}
-console.log(logs.all);
+} 
 var allLoggerTransport = new dailyRotateFile({
     name: 'all',
     filename:logs.all,
